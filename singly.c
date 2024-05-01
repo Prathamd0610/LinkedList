17bb1f520ec459cacbfd50508af53c454cc70e66
Prathamd0610 <prathamdhingra14@gmail.com>
2024-05-01 23:16:04 +0530

    singly linked list

--- singly.c
+++ singly.c
@@ -6,6 +6,8 @@ typedef struct linkedlist{
     struct linkedlist *next;
 }node;
 
+void delete_at_beg(node** head);
+void delete_at_end(node **head);
 void traverse(node *head);
 void insert_at_beg(node **head,int value);
 
@@ -13,15 +15,71 @@ void insert_at_end(node **head, int value);
 
 void main()
 {
-    int value;
+    int value,choice,s;
     node *head = NULL;                      //creating head of linkedlist
-    for(int i=0;i<10;i++){
-        printf("enter value:");
-        scanf("%d",&value);
-        // insert_at_beg(&head,value);
-        insert_at_end(&head,value);
+    while(1){
+        printf("1. insert at beginning :");
+        printf("2. insert at end :");
+        printf("3. insert after element :");
+        printf("4. delete at beginning :");
+        printf("5. delete at end :");
+        printf("6. delete after element :");
+        printf("7. delete entire list :");
+        printf("8. search :");
+        printf("9. traverse:");
+        printf("10. traverse in reverse:");
+        printf("11. reverse list:");
+        printf("12. exit:");
+        switch (choice)
+        {
+            case 1:
+                printf("enter element to insert: ");
+                insert_at_beg(&head,value);
+                break;
+            case 2:
+                printf("enter element to insert: ");
+                insert_at_end(&head,value);
+                break;
+            case 3:
+                printf("enter after which element to insert: ");
+                scanf("%d",&search);
+                printf("enter element to insert: ");
+                scanf("%d",&value);
+                insert_after_element(head,s,value);
+                break;
+            case 4:
+                printf("enter element to insert: ");
+                delete_at_beg(&head);
+                break;
+            case 5:
+                printf("enter element to insert: ");
+                delete_at_end(&head);
+                break;
+            case 6:
+                printf("enter after which element to delete: ");
+                scanf("%d",&search);
+                delete_after_element(head,s);
+                break;
+            case 7:
+                printf("not defined...");
+                break;
+            case 8:
+                printf("enter item to search: ");
+                search(head,s);
+                break;
+            case 9:
+                traverse(head);
+                break;
+            case 10:
+                printf("not defined...");
+                break;
+            case 11:
+                printf("not defined...");
+                break;
+            case 12:
+                exit(0);        
+        }
     }
-    traverse(head);
 }
 
 void insert_at_beg(node **head, int value){
@@ -62,5 +120,73 @@ void insert_at_end(node **head,int value){
             loc=loc->next;                  // locating the last node of linkedlist to add new node
         }
         loc->next=temp;                     // making previous last node to 2nd last node.
+    }
+}
+
+void delete_at_end(node **head){
+    node *temp,*loc;
+    if((*head)->next==NULL){
+        temp= *head;
+    }
+    else{
+        temp= *head;
+        while(temp->next!= NULL){
+            loc = temp;
+            temp= temp->next;
+        }
+        loc->next=NULL;
+    }
+    free(temp);
+}
+
+void delete_at_beg(node **head){
+    node *temp,*loc;
+    if((*head)->next==NULL){
+        temp=(*head);
+        (*head)=NULL;
+    }
+    else{
+        temp=(*head);
+        (*head) = temp->next;
+    }
+    free(temp);
+}
+
+node* search(node *head,int value){
+    while(head!=NULL){
+        if(head->info==value){
+            return head;
+        }
+    }
+}
+delete_after_element(node **head, int value){
+    node *temp,*loc;
+    loc=search(head,value);
+}
+
+void insert_after_element(node *head,int s,int value){
+    node *temp,*loc;
+    loc=search(head,s);
+    temp=(node*)malloc(sizeof(node));
+    temp->info=value;
+    if(loc->next==NULL){
+        temp->next=NULL;
+    }
+    else{
+        temp->next=loc->next;
+    }
+    loc->next=temp;
+}
+
+void delete_after_element(node *head,int s){
+    node *temp,*loc;
+    loc=search(head,s);
+    if(loc->next==NULL){
+        printf("not able to delete...");
+    }
+    else{
+        temp=loc->next;
+        loc->next=temp->next;
+        free(temp);
     }
 }
